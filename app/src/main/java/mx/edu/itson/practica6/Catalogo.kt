eedede8c0767c72eb2180589bd635e05073a3673
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-02-27 23:05:51 -0700

    pdf pg 22

---
+++ app/src/main/java/mx/edu/itson/practica6/Catalogo.kt
@@ -0,0 +1,11 @@
+package mx.edu.itson.practica6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Catalogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalogo)
+    }
+}
